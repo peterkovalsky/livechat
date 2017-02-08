b71107212eef8ffbab3312d8bc53df78b3eb3c5d
pkovalskyy <pkovalskyy@SYDPETER01>
2017-02-08 16:58:41 +1100

    geo location

---
+++ Kookaburra.Domain/Integration/IGeoLocator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kookaburra.Domain.Integration
+{
+    public interface IGeoLocator
+    {
+        void GetLocation(string ip);
+    }
+}
---
+++ Kookaburra.Domain/Integration/VisitorLocation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kookaburra.Domain.Integration
+{
+    public class VisitorLocation
+    {
+        public string Country { get; set; }
+
+        public string City { get; set; }
+    }
+}
