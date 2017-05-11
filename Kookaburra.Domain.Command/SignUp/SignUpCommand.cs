3c96e865c7e75a73b0f6cb0820441f433f2a565d
pkovalskyy <pkovalskyy@SYDPETER01>
2017-05-11 17:36:57 +1000

    sign up

---
+++ Kookaburra.Domain.Command/SignUp/SignUpCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kookaburra.Domain.Command.SignUp
+{
+    public class SignUpCommand : ICommand
+    {
+        public string OperatorIdentity { get; set; }
+
+        public string Account { get; set; }
+    }
+}
---
+++ Kookaburra.Domain.Command/SignUp/SignUpCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Kookaburra.Domain.Model;
+using Kookaburra.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kookaburra.Domain.Command.SignUp
+{
+    public class SignUpCommandHandler : ICommandHandler<SignUpCommand>
+    {
+        private readonly KookaburraContext _context;
+
+        public SignUpCommandHandler(KookaburraContext context)
+        {
+            _context = context;
+        }
+
+        public void Execute(SignUpCommand command)
+        {
+            var account = new Account
+            {
+
+            };
+            _context.Accounts.Add(account);
+        }
+    }
+}
